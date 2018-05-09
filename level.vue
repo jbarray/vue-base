494d3d5dc89527b3d090e4ea4ce574caa94d1539
jby <1614963384@qq.com>
2018-05-09 13:59:12 +0800

    评分组件

---
+++ level.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul class="level-wrapper">
+        <li v-bind:class="[level>0?'effectiveLevel':'unEffectiveLevel']">1</li><li v-bind:class="[level>1?'effectiveLevel':'unEffectiveLevel']">2</li><li v-bind:class="[level>2?'effectiveLevel':'unEffectiveLevel']">3</li>
+    </ul>
+</template>
+
+<script type="text/ecmascript-6">
+
+  export default {
+    props:{
+      level:{
+          default:0,
+          type:Number
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    /*设置li样式*/
+    .level-wrapper li{
+        display: inline-block;
+        padding: 0 5px;
+        border: 1px solid gainsboro;
+        margin-right: 1px;
+    }
+    /*有效评分样式*/
+    .effectiveLevel{
+        background-color: #EE7A23;
+        color: white;
+    }
+    /*无效评分样式*/
+    .unEffectiveLevel{
+        background-color: ghostwhite;
+        color: black;
+    }
+</style>
